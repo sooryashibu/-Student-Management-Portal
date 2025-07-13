b6412a75d8c5102ffca10a47c3bfc1a0a12fb3e5
sooryashibu <36664818+sooryashibu@users.noreply.github.com>
2025-07-13 16:02:31 +0200

    Delete src/api directory

--- src/api/studentsApi.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://jsonplaceholder.typicode.com/users";
-
-export const fetchStudents = () => axios.get(API_URL);
-export const fetchStudentById = (id) => axios.get(`${API_URL}/${id}`);
